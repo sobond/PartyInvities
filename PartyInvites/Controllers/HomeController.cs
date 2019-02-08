f1cfda1ef8fa0996f06f7dd07dd68bdb7a4c2f64
Daniel Soboń <Daniel.Sobon@bzwbk.pl>
2019-02-08 20:42:03 +0100

    export na git

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 17 ? "Dzień dobry" : "Dobry wieczór";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                //do zrobienia: wyślij zawartość guestResponse do organizatora przyjęcia
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                //blad kontroli poprawnosci wiec wyswietlamy ponownie formularz wprowadzania danych
+                return View();
+            }
+        }
+    }
+}
---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Proszę podać swoje imię i nazwisko.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać adres e-mail.")]
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage = "Proszę podać prawidłowy adres e-mail.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać numer telefonu.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Proszę określić, czy weźmiesz udział.")]
+        public bool? WillAtend { get; set; }
+    }
+}
